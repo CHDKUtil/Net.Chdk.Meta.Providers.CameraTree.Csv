627cb41567659fd2e8fd41a0e0650f41365ec466
Dmitry Shechtman <damnian@gmail.com>
2017-07-07 01:39:44 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.CameraTree.Csv/CsvCameraTreeProvider.cs
@@ -0,0 +1,15 @@
+﻿using Net.Chdk.Meta.Model.CameraTree;
+using Net.Chdk.Meta.Providers.CameraModel.Csv;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.CameraTree.Csv
+{
+    sealed class CsvCameraTreeProvider : CsvCameraProvider<TreePlatformData, TreeRevisionData, TreeSourceData>, ICameraTreeProvider
+    {
+        public IDictionary<string, TreePlatformData> GetCameraTree(Stream stream)
+        {
+            return GetCameras(stream);
+        }
+    }
+}
